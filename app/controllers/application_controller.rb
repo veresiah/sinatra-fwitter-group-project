1d05e5914e2c59c74bfced87046e6d04e511e45b
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-08 15:55:09 -0500

    commit tree 455185c42853283bd8e0dcfdb2a8c78da69ce228

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,21 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions 
+    set  :session_secret, "private_fwitter"
   end
 
+  get '/' do
+    erb :index
+  end 
+
+  helpers do 
+    def logged_in?
+      !!current_user
+    end 
+
+    def current_user 
+      @current_user ||= User.find_by_id(id: session[:user_id]) if session[:user_id]
+    end  
+  end 
 end
